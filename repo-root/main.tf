062a5667eeea21794ddda751381fc388176a3853
jgorrepati-blunova <jgorrepati@blunova.com>
2025-12-14 18:29:35 +0530

    LaunchEC2Instance

--- repo-root/main.tf
+++ repo-root/main.tf
@@ -31,11 +31,19 @@ resource "aws_key_pair" "key_pair" {
   public_key = tls_private_key.rsa_4096.public_key_openssh
 }
 
-# ⚠️ learning only — remove later
+# ⚠️ learning only — remove in real setups
 resource "local_file" "private_key" {
   content         = tls_private_key.rsa_4096.private_key_pem
   filename        = var.key_name
   file_permission = "0400"
 }
 
-resource "aws_instance_
+resource "aws_instance" "public_instance" {
+  ami           = "ami-0ecb62995f68bb549"
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.key_pair.key_name
+
+  tags = {
+    Name = "EC2_instance"
+  }
+}
