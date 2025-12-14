c6cdc4d35a822907ec81b9cec9ec78ca098de3d9
jgorrepati-blunova <jgorrepati@blunova.com>
2025-12-14 19:08:25 +0530

    Added TerraformDestroy

--- main.tf
+++ main.tf
@@ -4,12 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
-    tls = {
-      source = "hashicorp/tls"
-    }
-    local = {
-      source = "hashicorp/local"
-    }
   }
 }
 
@@ -18,32 +12,17 @@ provider "aws" {
 }
 
 variable "key_name" {
-  type = string
+  type    = string
+  default = "github-actions-key"
 }
 
-resource "tls_private_key" "rsa_4096" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "aws_key_pair" "key_pair" {
-  key_name   = var.key_name
-  public_key = tls_private_key.rsa_4096.public_key_openssh
-}
-
-# ⚠️ learning only — remove in real setups
-resource "local_file" "private_key" {
-  content         = tls_private_key.rsa_4096.private_key_pem
-  filename        = var.key_name
-  file_permission = "0400"
-}
-
-resource "aws_instance" "public_instance" {
+resource "aws_instance" "ephemeral_ec2" {
   ami           = "ami-0ecb62995f68bb549"
   instance_type = "t3.micro"
-  key_name      = aws_key_pair.key_pair.key_name
+  key_name      = var.key_name
 
   tags = {
-    Name = "EC2_instance"
+    Name = "ephemeral-ec2"
+    Owner = "github-actions"
   }
 }
